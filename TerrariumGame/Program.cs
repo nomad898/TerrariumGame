9d5c41e6957afc9bbe98a184d9d237b9f12cd1aa
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2017-12-06 17:57:21 +0600

    Initial commit

--- TerrariumGame/Program.cs
+++ TerrariumGame/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TerrariumGame.Models;
 
@@ -9,12 +10,24 @@ namespace TerrariumGame
 {
     class Program
     {
+        private static bool gameIsRunning = true;
+
         static void Main(string[] args)
+        {
+            while (gameIsRunning)
+            {
+                Console.Clear();
+                InitGame();
+                Thread.Sleep(1000);
+            }
+            Console.ReadKey(true);
+
+        }
+
+        static void InitGame()
         {
             Map world = new Map(10, 30);
             world.ShowMap();
-            Console.ReadKey(true);
-
         }
     }
 }
