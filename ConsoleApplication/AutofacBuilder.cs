88915589a199bbfb3cc2571a4fe708d12bc89be0
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2018-01-03 17:44:21 +0600

    add Autofac to main project

---
+++ ConsoleApplication/AutofacBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using InterfaceLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGame.Infrastructure;
+using TerrariumGame.Infrastructure.Factory;
+
+namespace ConsoleApplication
+{
+    class AutofacBuilder
+    {
+        public static IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Map>().As<IMap>();
+            builder.RegisterType<GameObjectFactory>().As<IGameObjectFactory>();
+            builder.RegisterType<MapManipulator>().As<IMapManipulator>();
+            builder.RegisterType<Dice>().As<IDice>();
+            builder.RegisterType<Game>().As<IGame>();
+
+            return builder.Build();
+        }
+    }
+}
--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using InterfaceLibrary.Interfaces;
+﻿using Autofac;
+using InterfaceLibrary.Interfaces;
 using System;
 using TerrariumGame.Infrastructure;
 using TerrariumGame.Infrastructure.Factory;
@@ -7,8 +8,11 @@ namespace ConsoleApplication
 {
     class Program
     {
+        private static IContainer Container { get; set; }
+
         static void Main(string[] args)
         {
+            IContainer container = AutofacBuilder.Build();
             IMap map = new Map(10, 10);
             IGameObjectFactory goFactory = new GameObjectFactory();
             IMapManipulator mapManipulator = new MapManipulator(map, goFactory, 4, 12, 1, 6);
@@ -17,5 +21,6 @@ namespace ConsoleApplication
             game.Start();
             Console.ReadKey(true);
         }
+
     }
 }
