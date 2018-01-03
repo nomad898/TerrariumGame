16f8c5080a0fe40b8604d9b81723c815c1f4aec3
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2018-01-03 16:48:45 +0600

    add training autofac example

--- AutofacTraining/Program.cs
+++ AutofacTraining/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,27 @@ namespace AutofacTraining
 {
     class Program
     {
+        private static IContainer Container { get; set; }
+
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            WriteDate();
+
+            Console.ReadKey(true);
+        }
+
+        private static void WriteDate()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
         }
     }
 }
