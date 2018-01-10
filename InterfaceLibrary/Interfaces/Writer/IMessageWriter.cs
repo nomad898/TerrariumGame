9025e76d3a65ee36f3e723c50d90a6cab37d84ac
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2018-01-10 17:56:01 +0600

    IMessageWriter created

---
+++ InterfaceLibrary/Interfaces/Writer/IMessageWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceLibrary.Interfaces.Writer
+{
+    interface IMessageWriter
+    {
+        string WriteMessage(string message);
+    }
+}
