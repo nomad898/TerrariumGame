8b5a6983847a19dfb86444f2916a07d9ac890741
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2018-02-16 16:07:07 +0600

    Commit

--- BusinessLibrary/Clients/WebApiClient.cs
+++ BusinessLibrary/Clients/WebApiClient.cs
@@ -12,7 +12,7 @@ namespace BusinessLibrary.Clients
             {
                 client.Headers.Add("Content-Type:application/json");
                 client.Headers.Add("Accept:application/json");
-                var result = client.DownloadString("http://localhost:50808/api/Value");
+                var result = client.DownloadString("http://localhost:50808/api/Conversation");
                 Console.WriteLine(Environment.NewLine + result);
                 Console.ReadKey();
             }
