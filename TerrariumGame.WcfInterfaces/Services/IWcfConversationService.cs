14f97e5efe634d68a7abf6dfb852e1ee63fbd259
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2018-03-14 17:07:31 +0600

    commit

---
+++ TerrariumGame.WcfInterfaces/Services/IWcfConversationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGame.Dto.DTO;
+
+namespace TerrariumGame.WcfInterfaces.Services
+{
+    [ServiceContract]
+    public interface IWcfConversationService
+    {
+        [OperationContract]
+        string Sum(int value, int b);
+
+        [OperationContract]
+        IEnumerable<ConversationDto> Get();
+    }
+}
---
+++ TerrariumGame.WcfService.Host/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGame.WcfServices;
+using TerrariumGame.WcfServices.Services;
+
+namespace TerrariumGame.WcfService.Host
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(WcfConversationService));
+            host.Open();
+            Console.WriteLine("Service hosted successfully");
+            Console.ReadKey(true);
+        }
+    }
+}
