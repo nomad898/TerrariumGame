3ceee988af059b2ff05d860d43b7f825f1718fff
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2018-04-11 14:30:31 +0600

    Trying to do reflection

--- TerrariumGame.PluginApp/Program.cs
+++ TerrariumGame.PluginApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InterfaceLibrary.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -12,22 +13,28 @@ namespace TerrariumGame.PluginApp
     {
         static void Main(string[] args)
         {
-            Assembly clPath = Assembly.LoadFile(@"C:\Users\Amirzhan_Alimov\Source\Repos\TerrariumGame\TerrariumGame.PluginLibrary\bin\Debug\TerrariumGame.PluginLibrary.dll");
+            Assembly clPath = Assembly.LoadFile(@"C:\Users\Amirzhan_Alimov\Source\Repos\TerrariumGame\TerrariumGame\bin\Debug\TerrariumGame.dll");
 
-            Type parentType = typeof(IPlugin);
+            Type parentType = typeof(IWorker);
 
             var x = clPath.GetTypes().Where(t => t.GetInterfaces().Contains(parentType));
 
             foreach (var item in x)
             {
                 object instance = Activator.CreateInstance(item);
-                PropertyInfo prop = item.GetProperty("Order");
-                prop.SetValue(instance, 5);
 
-                Console.WriteLine("xxx" + prop.GetValue(instance));
-            }   
-            
+                
+                MethodInfo method = item.GetMethod("Say", BindingFlags.Instance | BindingFlags.NonPublic,
+        Type.DefaultBinder,
+        new[] { typeof(int), typeof(int) },
+        null);
+
+                object result = method.Invoke(instance, new object[] { "Hello" });
+                Console.WriteLine(result);
+
+            }
+
             Console.ReadKey(true);
-        }        
+        }
     }
 }
