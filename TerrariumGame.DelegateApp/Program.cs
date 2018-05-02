d79ff77e8cc775431307a2d5c243b2f889b109ab
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2018-05-02 14:15:53 +0600

    I've started delegate task

---
+++ TerrariumGame.DelegateApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumGame.DelegateApp
+{
+    class Program
+    {
+        private delegate string ShowMessage(string message);
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
