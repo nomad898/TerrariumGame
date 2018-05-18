78a84a07f0fcde3dd356df1f8cd5f462533351e3
Alimov Amirzhan <Amirzhan_Alimov@epam.com>
2018-05-18 11:34:05 +0600

    added anonymous methods

---
+++ TerrariumGame.AnonymousMethodsApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumGame.AnonymousMethodsApp
+{
+
+    class Program
+    {
+        delegate int FibonacciDel(int i);
+
+        static void Main(string[] args)
+        {
+            FibonacciDel anonymousDel = delegate (int i)
+            {
+                return i > 1 ?
+                    Fibonacci(i - 1) + Fibonacci(i - 2) : i;
+            };
+
+            Console.WriteLine(anonymousDel(7));
+
+            FibonacciDel lambdaDel = (int i) =>
+            {
+                return i > 1 ?
+                    Fibonacci(i - 1) + Fibonacci(i - 2) : i;
+            };
+
+            Console.WriteLine(lambdaDel(7));
+
+            Console.WriteLine(Fibonacci(7));
+
+            Console.ReadKey(true);
+        }
+
+        public static int Fibonacci(int i)
+        {
+            return i > 1 ?
+                Fibonacci(i - 1) + Fibonacci(i - 2) : i;
+        }
+    }
+}
