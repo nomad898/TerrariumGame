3a093a3cdb76ad127f36169270c53f655072ec81
Amirzhan Alimov <Amirzhan_Alimov@epam.com>
2018-08-03 16:09:38 +0600

    Deleted comments in GameRunner.AutofacBuilder

--- TerrariumGame.GameRunner/AutofacBuilder.cs
+++ TerrariumGame.GameRunner/AutofacBuilder.cs
@@ -11,16 +11,6 @@ namespace TerrariumGame.GameRunner
         public static IContainer Build()
         {
             var builder = new ContainerBuilder();
-            //REFERENCE DELETED!!!
-            //This is works, but should i use it?
-            //builder.Register(m => new Map(10, 10))
-            //    .As<IMap>();
-            //builder.RegisterType<Map>().As<IMap>().SingleInstance();
-            //builder.RegisterType<Dice>().As<IDice>().SingleInstance();
-            //builder.RegisterType<GameObjectFactory>().As<IGameObjectFactory>().SingleInstance();
-            //builder.RegisterType<MapManipulator>().As<IMapManipulator>().SingleInstance();       
-            //builder.RegisterType<ConsoleMessageWriter>().As<IMessageWriter>().SingleInstance();
-            //builder.RegisterType<Game>().As<IGame>().SingleInstance();
             return builder.Build();
         }
 
